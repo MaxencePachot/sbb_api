73f3a320e41e6923dd46d0713b40f3f25923ad9d
MaxencePachot <pachot.maxence@gmail.com>
2025-09-01 22:41:29 +0200

    refactoring variable name, add a csv reader, add parse argument, clean the code

---
+++ weather_data/reader_weather_data.py
@@ -0,0 +1,24 @@
+from adls_connection import get_adls_client
+import pandas as pd
+from io import BytesIO
+
+def print_csv_from_adls(adls_folder: str, filename: str):
+    """
+    Download a CSV file from ADLS and display its contents as a pandas DataFrame
+    """
+    _, fs_client = get_adls_client()
+    adls_path = f"{adls_folder}/{filename}"
+    file_client = fs_client.get_file_client(adls_path)
+
+    download = file_client.download_file()
+    content = download.readall() # give a bytes file so we have to convert it after
+
+    df = pd.read_csv(BytesIO(content), sep=";") # Convert in csv
+    
+    # Display parameters
+    pd.set_option("display.max_columns", None)
+    pd.set_option("display.width", 1000)
+    pd.set_option("display.max_colwidth", 10)
+
+    print(f"Top 5 data in {filename} :")
+    print(df.head(5))
